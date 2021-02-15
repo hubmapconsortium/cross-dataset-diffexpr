0826083e5cd5ed72859e2de602c59a24a9e86fe2
Sean <Sean.Donahue.5311@gmail.com>
2021-02-15 15:39:17 -0500

    Fixes to batch correction and handling of unfiltered data, clean-up
    Adding call to function for tarring, zipping, and scping outputs, adding additional necessary parameters to pipeline
    
    Adding new parameter to function call

--- bin/find_marker_genes.py
+++ bin/find_marker_genes.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from pathlib import Path
-from cross_dataset_common import get_pval_dfs, make_quant_df, create_minimal_dataset
+from cross_dataset_common import get_pval_dfs, make_quant_df, create_minimal_dataset, tar_zip_scp
 import anndata
 import pandas as pd
+from hubmap_cell_id_gen_py import get_sequencing_cell_id
 
-def main(h5ad_file: Path, old_cluster_file:Path):
+
+def main(h5ad_file: Path, old_cluster_file:Path, known_hosts_file:Path):
     adata = anndata.read_h5ad(h5ad_file)
-    adata.obs['cell_id'] = adata.obs.index
+    cell_id_list = [get_sequencing_cell_id(adata.obs["dataset"][i], adata.obs["barcode"][i]) for i in adata.obs.index]
+    adata.obs["cell_id"] = pd.Series(cell_id_list, index=adata.obs.index)
 
-    cell_df = adata.obs.copy()
-    cell_df = cell_df[['cell_id', 'barcode', 'dataset', 'tissue_type', 'modality', 'dataset_leiden', 'leiden']].astype(str)
-
-    quant_df = make_quant_df(adata)
-    quant_df.to_csv('rna.csv')
-
-    organ_df, cluster_df = get_pval_dfs(adata, 'rna')
-
-    print(cluster_df['dataset'].unique())
+    #This can be threaded in the cdcommon lib
+    organ_df, cluster_df = get_pval_dfs(adata)
 
     with pd.HDFStore(old_cluster_file) as store:
         old_cluster_df = store.get('cluster')
 
-    cluster_df = pd.concat([old_cluster_df, cluster_df])
+    cluster_df_list = cluster_df.to_dict(orient='records')
+    cluster_df_list.extend(old_cluster_df.to_dict(orient='records'))
 
-    print(cluster_df['dataset'].unique())
+#    cluster_df = pd.concat([old_cluster_df, cluster_df])
+    cluster_df = pd.DataFrame(cluster_df_list)
+
+    cell_df = adata.obs.copy()
+    clusters_list = [",".join([cell_df["leiden"][i], cell_df["dataset_leiden"][i]]) for i in cell_df.index]
+    cell_df["clusters"] = pd.Series(clusters_list, index=cell_df.index)
+    cell_df = cell_df[['cell_id', 'barcode', 'dataset', 'organ', 'modality', 'clusters']]
+
+    adata.X = adata.raw.X
+
+    quant_df = make_quant_df(adata)
+    quant_df.to_csv('rna.csv')
 
     with pd.HDFStore('rna.hdf5') as store:
         store.put('cell', cell_df, format='t')
@@ -33,10 +41,13 @@ def main(h5ad_file: Path, old_cluster_file:Path):
 
     create_minimal_dataset(cell_df, quant_df, organ_df, cluster_df, 'rna')
 
+    tar_zip_scp("rna", known_hosts_file)
+
 if __name__ == '__main__':
     p = ArgumentParser()
     p.add_argument('bc_h5ad_file', type=Path)
     p.add_argument('old_cluster_file', type=Path)
+    p.add_argument('known_hosts_file', type=Path)
     args = p.parse_args()
 
-    main(args.bc_h5ad_file, args.old_cluster_file)
+    main(args.bc_h5ad_file, args.old_cluster_file, args.known_hosts_file)
